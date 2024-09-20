6f5d2ea5932663f905c4b97d5d94ab3c4bd0ad02
codewithzain099 <zainiikhan099@gmail.com>
2024-09-20 10:21:32 -0700

    app posts route has been added

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response } from "express";
 import dotenv from "dotenv";
+import axios from "axios";
 
 // Load environment variables from .env file
 dotenv.config();
@@ -18,6 +19,16 @@ app.get("/", (req: Request, res: Response) => {
 // Basic route
 app.get("/blogs", (req: Request, res: Response) => {
   res.send("Hello from the and the blogs app!");
+});
+// Basic route
+app.get("/posts", async (req: Request, res: Response) => {
+  const response = await axios.get(
+    "https://jsonplaceholder.typicode.com/posts"
+  );
+  return res.status(200).json({
+    status: true,
+    posts: response?.data,
+  });
 });
 
 // Start server
